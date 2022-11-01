a47077fd03632a9a9cd751281996739b5b370029
Iris2783 <iris.corp.2783@gmail.com>
2022-11-02 00:07:47 +0900

    serviceの内容を追加

--- json_sample5_responsive/lib/service/remote_service_users.dart
+++ json_sample5_responsive/lib/service/remote_service_users.dart
@@ -1,1 +1,20 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import '../model/user.dart';
+import 'package:http/http.dart' as http;
+
+List<Users> parseUsers(String responseBody) {
+  var list = json.decode(responseBody) as List<dynamic>;
+  List<Users> users = list.map((model) => Users.fromJson(model)).toList();
+  return users;
+}
+
+Future<List<Users>> fetchUsers() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+  if (response.statusCode == 200) {
+    return compute(parseUsers, response.body);
+  } else {
+    throw Exception('Request API Error');
+  }
+}
 
