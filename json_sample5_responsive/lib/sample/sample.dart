aee3f2f3fa400aa58339ffef5fc127e7ee2c1d24
Iris2783 <iris.corp.2783@gmail.com>
2022-11-06 23:14:20 +0900

    ページ遷移用サンプル

---
+++ json_sample5_responsive/lib/sample/sample.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SamplePage extends StatelessWidget {
+  const SamplePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SMAPLE'),
+        ),
+        body: const Center(
+          child: Text('Sample Page'),
+        ),
+      ),
+    );
+  }
+}
