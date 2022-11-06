a5ccbc456b996572a203a9beff79f6b0cea5dbc7
Iris2783 <iris.corp.2783@gmail.com>
2022-11-06 23:13:43 +0900

    コード追加

--- json_sample5_responsive/lib/homepage/desktop_scaffold.dart
+++ json_sample5_responsive/lib/homepage/desktop_scaffold.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:json_sample5_responsive/constants/constants.dart';
+import '../drawer/mydrawer.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
@@ -10,6 +12,21 @@ class DesktopScaffold extends StatefulWidget {
 class _DesktopScaffoldState extends State<DesktopScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: myAppBar,
+      body: Row(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              child: Column(
+                children: const [
+                  MyDrawer(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
