045c13ff1c306770aefe071ecc5c55d348006044
Iris2783 <iris.corp.2783@gmail.com>
2022-11-24 23:50:25 +0900

    Postレスポンシブのリストページの実装

--- json_sample5_responsive/lib/listpage_post/desktop_postlistpage.dart
+++ json_sample5_responsive/lib/listpage_post/desktop_postlistpage.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
+import 'package:json_sample5_responsive/sample/sample.dart';
+import 'package:json_sample5_responsive/service/remote_service_posts.dart';
 import '../drawer/mydrawer.dart';
 
 class DesktopPostListPage extends StatefulWidget {
@@ -13,7 +17,7 @@ class _DesktopPostListPageState extends State<DesktopPostListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DESKTOP'),
+        title: const Text('POST_Desktop'),
       ),
       body: Row(
         children: [
@@ -28,6 +32,84 @@ class _DesktopPostListPageState extends State<DesktopPostListPage> {
               ),
             ),
           ),
+          Expanded(
+            child: Container(
+              child: FutureBuilder(
+                future: fetchPosts(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          padding: const EdgeInsets.all(10),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12),
+                                    color: Colors.grey[300],
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        snapshot.data[index].title,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        snapshot.data[index].body ?? '',
+                                        maxLines: 10,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SamplePage(
+                                      // snapshot.data[index],
+                                      ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    );
+                  } else if (snapshot.hasError) {
+                    return Container(
+                      child: const Center(
+                        child: Text('Not Found Data'),
+                      ),
+                    );
+                  } else {
+                    return Center(
+                      child: Container(
+                        child: const CircularProgressIndicator(),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          )
         ],
       ),
     );
--- json_sample5_responsive/lib/listpage_post/moblie_postlistpage.dart
+++ json_sample5_responsive/lib/listpage_post/moblie_postlistpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:json_sample5_responsive/sample/sample.dart';
+import 'package:json_sample5_responsive/service/remote_service_posts.dart';
 
 class MobilePostListPage extends StatefulWidget {
   const MobilePostListPage({super.key});
@@ -12,7 +14,82 @@ class _MobilePostListPageState extends State<MobilePostListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MOBILE'),
+        title: const Text('POST_Mobile'),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: fetchPosts(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Row(
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Colors.grey[300],
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  snapshot.data[index].title,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.data[index].body ?? '',
+                                  maxLines: 10,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SamplePage(
+                                // snapshot.data[index],
+                                ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Container(
+                child: const Center(
+                  child: Text('Not Found Data'),
+                ),
+              );
+            } else {
+              return Center(
+                child: Container(
+                  child: const CircularProgressIndicator(),
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
